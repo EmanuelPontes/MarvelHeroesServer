512f57fbe098ee779bd08112b6eead13a5b27bcd
EmanuelPontes <emanuelpp26@gmail.com>
2022-02-08 19:17:36 -0300

    fix: mixed content heroku

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ var logger = require('morgan');
 
 var app = express();
 
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
+  }
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
